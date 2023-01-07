8835aa89385af8d41e359bda90af8dd9f8619dbf
Andre Katov <andy.katov@icloud.com>
2023-01-08 00:03:15 +0200

    added stripe controller

---
+++ controllers/stripeController.js
@@ -0,0 +1,22 @@
+import asyncWrapper from "../middleware/asyncWrapper.js";
+import stripe from "stripe";
+import { StatusCodes } from "http-status-codes";
+
+const stripeController = asyncWrapper(async (req, res) => {
+  const stripeClient = stripe(process.env.STRIPE_API_KEY);
+  const { purchase, total_amount, shipping_fee } = req.body;
+
+  const calculateOrderAmount = () => {
+    return total_amount + shipping_fee;
+  };
+
+  const paymentIntent = await stripeClient.paymentIntents.create({
+    amount: calculateOrderAmount(),
+    currency: "usd",
+  });
+
+  console.log(paymentIntent);
+  res.json({ clientSecret: paymentIntent.client_secret });
+});
+
+export default stripeController;
