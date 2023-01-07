065060801990df295ca131d15d763832388452b4
Andre Katov <andy.katov@icloud.com>
2023-01-08 00:04:39 +0200

    added post route and few other minor changes

--- app.js
+++ app.js
@@ -3,27 +3,28 @@ import express from "express";
 import dotenv from "dotenv";
 import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import notFound from "./middleware/notFound.js";
-import { StatusCodes } from "http-status-codes";
-import asyncWrapper from "./middleware/asyncWrapper.js";
+import stripeController from "./controllers/stripeController.js";
+import cors from "cors";
 
 dotenv.config();
+
 const server = express();
 const PORT = process.env.PORT || 8000;
 
+// application specific middleware
+server.use(cors());
+server.use(express.json());
+server.use(express.static("./public"));
+
+// route
+server.post("/stripe", stripeController);
+
+// error handling middleware & not found middleware
+server.use(errorHandlerMiddleware);
+server.use(notFound);
+
 const start = () => {
   try {
-    // test route
-    server.get(
-      "/",
-      asyncWrapper((req, res, next) => {
-        res.status(StatusCodes.OK).send("Welcome to stripe project");
-      })
-    );
-
-    // error handling middleware & not found middleware
-    server.use(errorHandlerMiddleware);
-    server.use(notFound);
-
     http.createServer(server).listen(PORT, function () {
       console.info("Server is listening on:", this.address());
     });
